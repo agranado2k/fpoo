e6ea7fa634b242753a15372ad1c9608ba9be783b
Arthur Granado <agranado@pluga.co>
2017-01-28 14:18:57 -0200

    Chapter 1: Embedding an Object-Oriented Language. Section 4: All the Class in a Constructor

--- sources/embedded-functions.clj
+++ sources/embedded-functions.clj
@@ -12,8 +12,21 @@
         :y y
         :__class_symbol__ 'Point
         :__methods__ {
-           :class :__class_symbol__
-           :shift (fn [this xinc yinc]
-                    (make Point (+ (:x this) xinc)
-                                (+ (:y this) yinc)))}}))
+            :x :x,
+            :y :y,
+            :class :__class_symbol__
+            :shift (fn [this xinc yinc]
+                    (make Point (+ (send-to this :x) xinc)
+                                (+ (send-to this :y) yinc)))
+            :add (fn [this, point]
+                    (send-to this :shift (send-to point :x) 
+                                          (send-to point :y))
+              )}}))
 
+
+;; Test Cases
+(def my-point (make Point 1 2))
+(prn (send-to my-point :x))
+(prn (send-to my-point :y))
+(prn (send-to my-point :shift -1 -100))
+(prn (send-to my-point :add (make Point -1 -100)))
