f2a6df109ff4f543036d1320ec4450926d2cd68c
Arthur Granado <agranado@pluga.co>
2017-01-28 13:04:05 -0200

    Chapter 1: Embedding an Object-Oriented Language. Section 3: A Barely Believalbe Object - define function for Points and Triangles

--- sources/add-and-make.clj
+++ sources/add-and-make.clj
@@ -33,4 +33,52 @@
                                   (Point 0 10)
                                   (Point 10 0)))
 
+(def add 
+      (fn [point1 point2]
+        (Point (+ (x point1) (x point2))
+               (+ (y point1) (y point2)))))
 
+(def add
+      (fn [point1 point2]
+        (shift point1 (x point2) (y point2))))
+
+(def make
+      (fn [& args]
+        (apply (first args) (rest args))))
+
+(def equal-points?
+      (fn [point1 point2]
+          (and 
+            (= (x point1) (x point2))
+            (= (y point1) (y point2)))))
+
+(def equal-triangles? 
+      (fn [triangle1 triangle2]
+        (and
+          (equal-points? (:point1 triangle1) (:point1 triangle2))
+          (equal-points? (:point2 triangle1) (:point2 triangle2))
+          (equal-points? (:point3 triangle1) (:point3 triangle2)))))
+
+(def equal-points?
+      (fn [& points]
+          (and 
+            (apply = (map x points))
+            (apply = (map y points)))))
+
+(def equal-triangles? 
+      (fn [& triangles]
+        (and
+          (apply equal-points? (map :point1 triangles))
+          (apply equal-points? (map :point2 triangles))
+          (apply equal-points? (map :point3 triangles)))))
+
+(def valid-triangle?
+      (fn [point1 point2 point3]
+        (and
+          (not= point1 point2)
+          (not= point1 point3)
+          (not= point2 point3))))
+
+
+
+        
