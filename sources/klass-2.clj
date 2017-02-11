f46c9eeb39869a2621cde218c4eb928e14b3252f
Arthur Granado <agranado@pluga.co>
2017-02-11 15:26:13 -0200

    V To Ruby - Chapter 18: The Class that makes classes

--- sources/klass-2.clj
+++ sources/klass-2.clj
@@ -95,3 +95,94 @@
           :origin (fn [class] (send-to class :new 0 0))
          })
 
+;;Exercise 1
+
+(install (basic-class 'Klass,
+                      :left 'MetaKlass,
+                      :up 'Anything,
+                      {
+                       :new
+                       (fn [class & args]
+                         (let [seeded {:__class_symbol__ (:__own_symbol__ class)}]
+                           (apply-message-to class seeded :add-instance-values args)))
+                       :to-string (fn [this] (str "class " (:__own_symbol__ this)))
+                      }))
+
+;;Prove
+(send-to Point :to-string)
+;;"class Point"
+(send-to Klass :to-string)
+;;"class Klass"
+;; As before:
+(send-to (send-to Anything :new) :to-string)
+;;"{:__class_symbol__ Anything}"
+(send-to (send-to Point :new 1 2) :to-string)
+;;"{:y 2, :x 1, :__class_symbol__ Point}"
+
+;; Exercise 2
+
+;; I'll mark classes invisible by tagging them with metadata.
+
+(def invisible
+     (fn [class]
+       (assoc class :__invisible__ true)))
+
+(def invisible?
+     (fn [class-symbol] (:__invisible__ (eval class-symbol))))
+
+;; Change the already-defined metaclasses to be invisible:
+
+(def MetaAnything (invisible MetaAnything))
+(def MetaKlass (invisible MetaKlass))
+(def MetaPoint (invisible MetaPoint))
+
+;; Ancestors just removes invisible classes from the
+;; reversed lineage.
+
+(def Klass
+     (assoc-in Klass
+               [:__instance_methods__ :ancestors]
+               (fn [class]
+                 (remove invisible?
+                         (reverse (lineage (:__own_symbol__ class)))))))
+
+;; New metaclasses need to be created to be invisible.
+
+(def MetaKlass
+     (assoc-in MetaKlass
+               [:__instance_methods__ :new]
+                (fn [this
+                     new-class-symbol superclass-symbol
+                     instance-methods class-methods]
+                  ;; Metaclass
+                  (install
+                   ;; VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV     new
+                   (invisible
+                    ;; ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
+                    (basic-class (metasymbol new-class-symbol)
+                                 :left 'Klass
+                                 :up 'MetaAnything
+                                 class-methods)))
+                  ;; Class
+                  (install
+                   (basic-class new-class-symbol
+                                :left (metasymbol new-class-symbol)
+                                :up superclass-symbol
+                                instance-methods)))))
+
+;;Prove
+(send-to Point :ancestors)
+;;(Point Anything)
+(send-to ColoredPoint :ancestors)
+;;(ColoredPoint Point Anything)
+(send-to Klass :ancestors)
+;;(Klass Anything)
+(send-to MetaPoint :ancestors)
+;;(Klass Anything)
+
+;; Exercise 3
+
+
+;; Prove
+
+(send-to Point :class-name)
